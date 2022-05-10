7b1128227073983c1172862037f8a7559a354ebd
piyanatnine <56292153+piyanatnine@users.noreply.github.com>
2022-05-10 17:21:21 +0700

    frontend multi

---
+++ frontend/src/App.vue
@@ -0,0 +1,72 @@
+<template>
+  <div id="app">
+    <div class="bg-[#875D53] h-[50vh]">
+    <div class="w-80">
+      <multiselect 
+        v-model="value" 
+        tag-placeholder="Add this as new tag" placeholder="Search or add a tag" 
+        :options="faculity" :multiple="true" 
+        :taggable="true" @tag="addTag"
+        :close-on-select="false">
+      </multiselect>
+    </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+import Multiselect from "vue-multiselect";
+import axios from 'axios'
+
+export default {
+  name: "App",
+  components: { Multiselect },
+  data() {
+    return {
+      value: null,
+      faculity: [],
+      round: [],
+      projects: [],
+      course: [],
+      all: [],
+      count: 0
+    };
+  },
+  methods: {
+    addTag (newTag) {
+      const tag = {
+        name: newTag,
+        code: newTag.substring(0, 2) + Math.floor((Math.random() * 10000000))
+      }
+      this.options.push(tag)
+      this.value.push(tag)
+    },
+    getData(){
+      axios.get('http://localhost:3001/getSelection')
+      .then(response => {
+        this.faculity = response.data[0].faculity
+        this.round = response.data[0].round
+        this.projects = response.data[0].project
+        this.course = response.data[0].course
+      })
+    },
+    getProject(){
+      axios.get('http://localhost:3001/getResult')
+      .then(response => {
+        this.all = response.data
+        this.count = this.all.length
+        console.log(response.data)
+      })
+    }
+  },
+  created() {
+    this.getData()
+    this.getProject()
+  }
+};
+</script>
+
+<style>
+ @import url("https://unpkg.com/vue-multiselect@2.1.0/dist/vue-multiselect.min.css");
+</style>
